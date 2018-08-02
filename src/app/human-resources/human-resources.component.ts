233e71d1c3386dbabaa277d49baff556a63ae0dd
Arber Marleku <arber.marleku@gmail.com>
2018-08-02 15:29:44 +0200

    test\

--- src/app/human-resources/human-resources.component.ts
+++ src/app/human-resources/human-resources.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HumanResourcesComponent implements OnInit {
 
+  myEmployee: any[] = [
+    {
+      id: '10001',
+      name: 'Arber Marleku',
+      username: 'amarleku',
+      telephone: '+38349600432',
+      position: 'CEO'
+    },
+    {
+      id: '10001',
+      name: 'Petrit Berisha',
+      username: 'pberisha',
+      telephone: '+38349499494',
+      position: 'CFO'
+    },
+    {
+      id: '10001',
+      name: 'Dorina Marku',
+      username: 'dmarku',
+      telephone: '+38349700454',
+      position: 'CFO'
+    }
+  ];
+  title = 'Testing';
+
   constructor() { }
 
   ngOnInit() {
